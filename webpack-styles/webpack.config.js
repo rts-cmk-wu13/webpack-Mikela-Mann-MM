50ac5529813e968235ae4060c677696f0c2bd0a5
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-03-20 13:32:24 +0100

    webpack base tilføjet

---
+++ webpack-styles/webpack.config.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    plugins: [new MiniCssExtractPlugin()],
+
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                    "postcss-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+
+    devtool: "source-map",
+};
